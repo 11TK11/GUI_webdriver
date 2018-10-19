4251df88421f126bab50bbfcb1347901fd35129d
Kevin Herrera <kevin.herrera@fundacion.jala.org>
2018-10-19 11:25:52 -0400

    sonarqube fix 3

--- e2e/specs/create.project.spec.js
+++ e2e/specs/create.project.spec.js
@@ -19,15 +19,13 @@ describe('pivotal tracker page create new project', () => {
     });
 
     after(() =>{
-        let projectID1;
-        //let projectID2;
-        //endpoint to get all user projects
+        let projectID;
         let response = browser.call(() => {return APIrequest.GetRequest('projects');});
         Object.values(response.data).map((project) => {
             if (projectData1.name === project.name) {
-                projectID1 = project.id;
+                projectID = project.id;
             }
         });
-        browser.call(() => {return APIrequest.DelRequest(`projects/${projectID1}`);});
+        browser.call(() => {return APIrequest.DelRequest(`projects/${projectID}`);});
     });
 });
