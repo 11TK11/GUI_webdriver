3a1347b022fba33f2a00a8c84f968c6cac7b4498
Carlos Gonzales <carlos.gonzales@jalasoft.com>
2018-10-23 15:32:00 -0400

    Updating cucumberOpts

--- wdio.conf.js
+++ wdio.conf.js
@@ -125,7 +125,7 @@ exports.config = {
     //
     // Make sure you have the wdio adapter package for the specific framework installed
     // before running any tests.
-    framework: 'mocha',
+    framework: 'cucumber',
     //
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
@@ -134,9 +134,33 @@ exports.config = {
     //
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
-    mochaOpts: {
-        ui: 'bdd',
-        timeout: 99999999
+    // mochaOpts: {
+    //     ui: 'bdd',
+    //     timeout: 99999999
+    // },
+    // If you are using Cucumber you need to specify where your step definitions are located.
+    // See also: https://github.com/webdriverio/wdio-cucumber-framework#cucumberopts-options
+    cucumberOpts: {
+        require: [
+            './src/steps/given.js',
+            './src/steps/then.js',
+            './src/steps/when.js',
+        ],        // <string[]> (file/dir) require files before executing features
+        backtrace: true,   // <boolean> show full backtrace for errors
+        compiler: [
+            'js:babel-register',
+        ],       // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
+        dryRun: false,      // <boolean> invoke formatters without executing steps
+        failFast: false,    // <boolean> abort the run on first failure
+        format: ['pretty'], // <string[]> (type[:path]) specify the output format, optionally supply PATH to redirect formatter output (repeatable)
+        colors: true,       // <boolean> disable colors in formatter output
+        snippets: true,     // <boolean> hide step definition snippets for pending steps
+        source: true,       // <boolean> hide source URIs
+        profile: [],        // <string[]> (name) specify the profile to use
+        strict: true,      // <boolean> fail if there are any undefined or pending steps
+        tags: [],           // <string[]> (expression) only execute the features or scenarios with tags matching the expression
+        timeout: 20000,      // <number> timeout for step definitions
+        ignoreUndefinedDefinitions: false, // <boolean> Enable this config to treat undefined definitions as warnings.
     },
     //==========
     // allure
